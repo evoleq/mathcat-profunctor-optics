6f58d5a1c671db2acc604961e6681e28b5fcacce
drx <schmidt@alpha-structure.com>
2023-03-08 22:23:35 +0100

    Mathcat-Storage

--- src/commonTest/kotlin/org/evoleq/math/cat/profunctor/optic/lens/lens-test.kt
+++ src/commonTest/kotlin/org/evoleq/math/cat/profunctor/optic/lens/lens-test.kt
@@ -3,7 +3,9 @@ package org.evoleq.math.cat.profunctor.optic.lens
 import org.evoleq.math.cat.morphism.by
 import org.evoleq.math.cat.optic.lens.getter
 import org.evoleq.math.cat.optic.lens.setter
+import org.evoleq.math.cat.profunctor.light.AlgebraicLight
 import org.evoleq.math.cat.profunctor.light.CartesianLight
+import org.evoleq.math.cat.profunctor.optic.Optic
 import org.evoleq.math.cat.profunctor.optic.o
 import org.evoleq.math.cat.profunctor.optic.propagate
 import org.evoleq.math.cat.structure.x
@@ -29,7 +31,6 @@ class LensTest {
         val update = by(propagated.setter())(Data(0,""))
         val updated = update(1)
         assertEquals(1,updated.x)
-        
     }
     
     @Test
@@ -46,8 +47,9 @@ class LensTest {
             prof.first<BigData>() contraMap { data: BigData -> data.data x data } map { pair: Pair< Data, BigData> -> pair.second.copy(data = pair.first) }
         }
         
-        val lens = bigDataLens o dataXLens
-    
+        val lens: Optic<Int, Int, BigData, BigData> =
+            bigDataLens o dataXLens
+        
         val propagated = lens.propagate(CartesianLight.unRefracted<Int,Int>())
         
         val view = by(propagated.getter())
@@ -58,5 +60,84 @@ class LensTest {
         val bigData = setter(0)
         assertEquals(0, bigData.data.x)
         
+    }
+    data class Data(val x: Int, val y: String) {
+        companion object {
+            val x: Lens<Int,Int, Data, Data> = Lens { profunctor ->
+                profunctor.first<Data>() contraMap {
+                        data: Data -> data.x x data
+                } map {
+                        pair: Pair<Int, Data> -> pair.second.copy(x = pair.first)
+                }
+        
+            }
+            val y: Lens<String,String, Data, Data> = Lens { profunctor ->
+                profunctor.first<Data>() contraMap {
+                        data: Data -> data.y x data
+                } map {
+                        pair: Pair<String, Data> -> pair.second.copy(y = pair.first)
+                }
+        
+            }
+        }
+    }
+    data class BigData(val data: Data, val d: Double) {
+        companion object {
+            val data: Lens<Data, Data, BigData, BigData> = Lens { profunctor ->
+                profunctor.first<BigData>() contraMap { data: BigData ->
+                    data.data x data
+                } map { pair: Pair<Data, BigData> ->
+                    pair.second.copy(data = pair.first)
+                }
+        
+            }
+            val d: Lens<Double, Double, BigData, BigData> = Lens { profunctor ->
+                profunctor.first<BigData>() contraMap { data: BigData ->
+                    data.d x data
+                } map { pair: Pair<Double, BigData> ->
+                    pair.second.copy(d = pair.first)
+                }
+        
+            }
+        }
+    }
+    @Test fun `default lenses`() {
+        val xInBigDataLens = BigData.data o Data.x
+        xInBigDataLens.propagate(CartesianLight.unRefracted())
+        var (get: ()->Int, set:(Int)->BigData) = with((BigData(Data(0,""),5.0)) x xInBigDataLens.propagate(CartesianLight.unRefracted())) {
+            { by(second.getter())(first) } x {r:Int -> by(second.setter())(first)(r) }
+        }
+        
+        val x = get()
+        println(x)
+        assertEquals(0,x)
+        val newBigData = set(2)
+        assertEquals(2, newBigData.data.x)
+        
+        val (
+            g: (BigData)->((Lens<Data, Data, BigData, BigData>)->(()->Data)),
+            s: (Lens<Data, Data, BigData, BigData>)->((Data)->((BigData)->BigData))
+        ) =  Pair({
+                bigData: BigData -> {
+                    lens: Lens<Data, Data, BigData, BigData>-> {
+                        by(lens.propagate(CartesianLight.unRefracted()).getter())(bigData)
+                    }
+                }
+            },  {
+                lens: Lens<Data, Data, BigData, BigData> -> {
+                    data: Data -> { bigData: BigData ->
+                        by(lens.propagate(CartesianLight.unRefracted()).setter())(bigData)(data)
+                    }
+                }
+            }
+        )
+        
+        fun <C, P> get(): (P)->((Lens<C, C, P, P>)->(()->C)) = { p -> { lens -> {
+            by(lens.propagate(CartesianLight.unRefracted()).getter())(p)
+            
+        }}}
+        fun <C, P> set(): (Lens<C, C, P, P>)->((C)->((P)->P)) = { lens -> { c -> { p ->
+            by(lens.propagate(CartesianLight.unRefracted()).setter())(p)(c)
+        }} }
     }
 }
